14c747142cf3912f9346a09d627454744f5d7e28
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-16 23:49:58 +0700

    feat: revamp labs demo

--- main.go
+++ main.go
@@ -1,58 +1,40 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
-
-type wrappedWriter struct {
-	http.ResponseWriter
-	Status int
-}
-
-func (w *wrappedWriter) WriteHeader(code int) {
-	w.ResponseWriter.WriteHeader(code)
-	w.Status = code
-}
-
-func logger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		writer := &wrappedWriter{
-			ResponseWriter: w,
-			Status:         http.StatusOK,
-		}
-		next.ServeHTTP(writer, r)
-		log.Printf("[%v] %v %v\n", writer.Status, r.Method, r.URL.Path)
-	})
-}
+import "net/http"
 
 func home(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusForbidden)
-	if _, err := w.Write([]byte("are you lost? try visiting /flag you might find something useful")); err != nil {
-		log.Printf("error writing response: %v", err)
-	}
+	w.Write([]byte("are you lost? try visiting /flag you might find something useful"))
 }
 
-func flag(w http.ResponseWriter, _ *http.Request) {
+func fake(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("what kind of flag? there is no flag here, i heard it's at /fl"))
+}
+
+func half(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write([]byte("flag: pai{p12aK7ikUm_w1r3sH4rk}")); err != nil {
-		log.Printf("error writing response: %v", err)
-	}
+	w.Write([]byte("wait so you really are looking for a flag? you should find more at /ag . here is the first half, flag: pai{p12aK7i"))
+}
+
+func flag(w http.ResponseWriter, _ *http.Request) { // flag: pai{p12aK7ikUm_w1r3sH4rk}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("here is the other half, flag: kUm_w1r3sH4rk}"))
 }
 
 func main() {
 	router := http.NewServeMux()
 	{
 		router.HandleFunc("GET /", home)
-		router.HandleFunc("GET /flag", flag)
+		router.HandleFunc("GET /flag", fake)
+		router.HandleFunc("GET /fl", half)
+		router.HandleFunc("GET /ag", flag)
 	}
 
 	server := http.Server{
 		Addr:    "0.0.0.0:1337",
-		Handler: logger(router),
+		Handler: router,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("failed to start server: %v", err)
-	}
+	server.ListenAndServe()
 }
